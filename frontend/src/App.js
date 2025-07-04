b8c25d1b7a962872c4f41da71eacd75a23a55ba5
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-07-04 15:29:43 +0530

    feat(frontend): add component that sets up the routing for the application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+
+import SignupForm from './components/SignupForm';
 import Homepage from './pages/homepage';
+import LoginForm from './components/LoginForm';
+import ViewAll from './pages/ViewAll';
+import AdminDashboard from './components/AdminDashboard'; // Import your AdminDashboard component
 
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+
+          {/* Signup page (set as default route) */}
+          <Route path="/" element={<Navigate to="/signup" replace />} />
+          <Route path="/signup" element={<SignupForm />} />
+
+          {/* Login page */}
+          <Route path="/login" element={<LoginForm />} />
+
+
+          {/* Homepage route */}
+          <Route path="/home" element={<Homepage />} />
+
+          {/* View all tasks page (should be passed props from Homepage if you want to share state) */}
+          <Route path="/tasks" element={<ViewAll />} />
+
+          {/* Admin Dashboard route */}
+          <Route path="/admin" element={<AdminDashboard />} />
+
+          {/* Catch-all: redirect to signup */}
+          <Route path="*" element={<Navigate to="/signup" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
